cc9f0802a7a6be1abaef2db9df0ff644c33dbc48
lebaoquan-ai <lebaoquan.aidev@gmail.com>
2025-07-21 20:39:02 +0700

    big overhaul

---
+++ src/data/site.js
@@ -0,0 +1,10 @@
+export default {
+    title: 'The Fool',
+    description:
+        'Hello, I am a fool. My name is Le Bao Quan, a software developer',
+    lang: 'en',
+    locale: 'en_US',
+    domain: 'lebaoquan-ai.github.io',
+    url: 'https://lebaoquan-ai.github.io',
+    repo: 'https://github.com/lebaoquan-ai/lebaoquan-ai.github.io'
+}
